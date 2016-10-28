1a4537940265a249673166b53ba1651d5d42a7c9
Larry Singleton <larrysingleton007@gmail.com>
2016-10-28 01:04:35 +0000

    User validatation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
   validates :name, presence: true
-  validates :email, presence: true
+  validates :password, presence: true
   validates :defaultBid, presence: true
+  
+ 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 100 },
+                    format: { with: VALID_EMAIL_REGEX }
+                
 end
